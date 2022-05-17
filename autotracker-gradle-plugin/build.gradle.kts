7cc574b5a3f5e5c6fa8df82efeef68ec41e0fea5
shenliming <348515494@qq.com>
2022-05-17 17:49:54 +0800

    update plugin version

--- autotracker-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/build.gradle.kts
@@ -37,7 +37,7 @@ val testPluginImplementation: Configuration by configurations.creating {
 }
 
 ext {
-    set("releaseVersion", "3.4.0")
+    set("releaseVersion", "3.4.0-SNAPSHOT")
     set("releaseVersionCode", 30400)
 }
 
