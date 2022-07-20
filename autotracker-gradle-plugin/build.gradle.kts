b369f8b35d6a24e2d7265ab25f7f2bfeb14db20e
shenliming <348515494@qq.com>
2022-07-20 14:50:49 +0800

    sensorsdata adapter 1. 版本更新为v3.4.1 2. TargetInject 兼容 !after 3. 添加sensor适配

--- autotracker-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/build.gradle.kts
@@ -21,8 +21,8 @@ val testPluginImplementation: Configuration by configurations.creating {
 }
 
 ext {
-    set("releaseVersion", "3.4.0")
-    set("releaseVersionCode", 30400)
+    set("releaseVersion", "3.4.1")
+    set("releaseVersionCode", 30401)
     set("agp_version", "7.2.1")
     set("low_agp_version", "4.2.2")
     set("kotlin_version", "1.6.21")
--- autotracker-gradle-plugin/src/main/kotlin/com/growingio/android/plugin/AutoTrackerPlugin.kt
+++ autotracker-gradle-plugin/src/main/kotlin/com/growingio/android/plugin/AutoTrackerPlugin.kt
@@ -52,11 +52,11 @@ abstract class AutoTrackerPlugin : Plugin<Project> {
 
         var inAndroidProject = false
 
-        val autoTrackerExtension = project.extensions.create("growingAutotracker", AutoTrackerExtension::class.java, instantiator)
+        val autoTrackerExtension =
+            project.extensions.create("growingAutotracker", AutoTrackerExtension::class.java, instantiator)
 
         project.plugins.withType(AndroidBasePlugin::class.java) {
             inAndroidProject = true
-
             if (SimpleAGPVersion.ANDROID_GRADLE_PLUGIN_VERSION < SimpleAGPVersion(4, 2)) {
                 // Configures bytecode transform using older APIs pre AGP 4.2
                 configureBytecodeTransform(project, autoTrackerExtension)
@@ -72,7 +72,7 @@ abstract class AutoTrackerPlugin : Plugin<Project> {
                 "The GrowingIO AutoTracker Gradle plugin can only be applied to an Android project"
             }
             LOG_ENABLE = autoTrackerExtension.logEnabled
-            initInjectClass(autoTrackerExtension.injectClasses, autoTrackerExtension.analyticsAdapter)
+            initInjectClass(autoTrackerExtension)
             checkAutoTrackerDependency(it, autoTrackerExtension.skipDependencyCheck)
         }
 
