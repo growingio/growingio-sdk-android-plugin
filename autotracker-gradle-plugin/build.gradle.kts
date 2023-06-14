38005d901e684da48eb4483224e5a13608582b2f
growingio <tianhui@growingio.com>
2023-06-14 12:07:46 +0800

    release 3.4.7

--- autotracker-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/build.gradle.kts
@@ -21,8 +21,8 @@ val testPluginImplementation: Configuration by configurations.creating {
 }
 
 ext {
-    set("releaseVersion", "3.4.6")
-    set("releaseVersionCode", 30406)
+    set("releaseVersion", "3.4.7")
+    set("releaseVersionCode", 30407)
     set("agp_version", "7.2.1")
     set("low_agp_version", "4.2.2")
     set("kotlin_version", "1.6.21")
