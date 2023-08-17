febf5a89ce8ba9026c3a8294137fa1bb304ebcb2
cpacm <shenliming1013@gmail.com>
2023-08-17 19:21:03 +0800

    Update build.gradle.kts
    version 3.5.1

--- autotracker-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/build.gradle.kts
@@ -3,8 +3,8 @@ buildscript {
         set("kotlin_version", "1.8.20")
         set("agp_version", "8.1.0")
         set("low_agp_version", "4.2.2")
-        set("releaseVersion", "3.5.0")
-        set("releaseVersionCode", 30500)
+        set("releaseVersion", "3.5.1")
+        set("releaseVersionCode", 30501)
     }
 }
 
