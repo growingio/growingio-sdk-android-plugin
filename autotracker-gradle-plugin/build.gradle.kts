6201ec8db04ea0d41433f3ec799f517025ee7547
shenliming <348515494@qq.com>
2023-09-11 16:17:22 +0800

    update plugin version to 4.0.0

--- autotracker-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/build.gradle.kts
@@ -3,11 +3,8 @@ buildscript {
         set("kotlin_version", "1.8.20")
         set("agp_version", "8.1.0")
         set("low_agp_version", "4.2.2")
-        set("releaseVersion", "3.5.3")
-        set("releaseVersionCode", 30503)
-
-        set("saasVersion", "2.10.0")
-        set("saasVersionCode", "21000")
+        set("releaseVersion", "4.0.0")
+        set("releaseVersionCode", 40000)
     }
 }
 
