ab9b1f5c5761e5c228491f4ce3542e12a4987787
shenliming <348515494@qq.com>
2023-09-27 09:39:51 +0800

    fix giokit-no-op

--- autotracker-gradle-plugin/src/main/kotlin/com/growingio/android/plugin/giokit/GioKitProcessor.kt
+++ autotracker-gradle-plugin/src/main/kotlin/com/growingio/android/plugin/giokit/GioKitProcessor.kt
@@ -203,6 +203,6 @@ object GioKitProcessor {
     }
 
     private const val GIOKIT_DEPEND = "com.growingio.giokit:giokit"
-    private const val GIOKIT_DEPEND_NOOP = "com.growingio.giokit-no-op:giokit"
+    private const val GIOKIT_DEPEND_NOOP = "com.growingio.giokit:giokit-no-op"
     private const val GIOKIT_DEFAULT_VERSION = "2.0.0"
 }
