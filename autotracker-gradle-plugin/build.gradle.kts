bbad1bbfbd951c2314d2d32f125ef2be97c99b5c
shenliming <348515494@qq.com>
2023-10-24 16:07:50 +0800

    fix test gradle version

--- autotracker-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/build.gradle.kts
@@ -73,10 +73,12 @@ dependencies {
     testImplementation(gradleTestKit())
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("com.android.tools.build:gradle:${rootProject.extra["low_agp_version"]}")
-    testImplementation(project(":agp-wrapper-42"))
+    testImplementation("org.ow2.asm:asm:9.5")
+    testImplementation("org.ow2.asm:asm-commons:9.5")
+    testImplementation("com.android.tools.build:gradle:7.4.2")
+    testImplementation(project(":agp-wrapper-72"))
 
-    testPluginImplementation("com.android.tools.build:gradle:${rootProject.extra["low_agp_version"]}")
+    testPluginImplementation("com.android.tools.build:gradle:7.4.2")
     testPluginImplementation("com.google.guava:guava:30.1.1-jre")
 }
 
