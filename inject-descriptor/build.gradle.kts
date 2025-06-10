30edef52f42f711668946ecfa59be864df49873b
cpacm <shenliming1013@gmail.com>
2025-06-10 14:47:53 +0800

    add jvmtoolchain

--- inject-descriptor/build.gradle.kts
+++ inject-descriptor/build.gradle.kts
@@ -25,6 +25,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    kotlin {
+        jvmToolchain(11)
+    }
 }
 
 dependencies {
