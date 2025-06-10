7d1af0f8ef98168cc00e0be7e6c784e5feed9804
cpacm <shenliming1013@gmail.com>
2025-06-10 14:33:31 +0800

    saas plugin version to 2.10.4

--- autotracker-gradle-plugin/saas-gradle-plugin/build.gradle.kts
+++ autotracker-gradle-plugin/saas-gradle-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
     extra.apply {
-        set("saasVersion", "2.10.3")
-        set("saasVersionCode", "21003")
+        set("saasVersion", "2.10.4")
+        set("saasVersionCode", "21004")
     }
 }
 
